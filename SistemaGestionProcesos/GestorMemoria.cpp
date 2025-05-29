390006bf40c124845c0d4694135d99fb373becb3
Giovany <giovanyhuamanimadueno8@gmail.com>
2025-05-28 21:12:33 -0500

    Add files via upload

---
+++ SistemaGestionProcesos/GestorMemoria.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct BloqueMemoria {
+    int idProceso;
+    int tam;
+    BloqueMemoria* siguiente;
+};
+
+BloqueMemoria* cima = NULL;
+
+void asignarMemoria() {
+    int id, tam;
+    cout << "ID Proceso: "; cin >> id;
+    cout << "Tamaño memoria: "; cin >> tam;
+    BloqueMemoria* nuevo = new BloqueMemoria{id, tam, cima};
+    cima = nuevo;
+    cout << "Memoria asignada al proceso " << id << ".\n";
+}
+
+void liberarMemoria() {
+    if (cima == NULL) {
+        cout << "No hay bloques en la pila.\n";
+        return;
+    }
+    cout << "Liberando memoria del proceso " << cima->idProceso << ".\n";
+    BloqueMemoria* temp = cima;
+    cima = cima->siguiente;
+    delete temp;
+}
+
+void mostrarMemoria() {
+    BloqueMemoria* temp = cima;
+    cout << "\n--- Pila de Memoria ---\n";
+    while (temp != NULL) {
+        cout << "Proceso: " << temp->idProceso << ", Tamaño: " << temp->tam << endl;
+        temp = temp->siguiente;
+    }
+}
---
+++ SistemaGestionProcesos/GestorMemoria.h
@@ -0,0 +1,8 @@
+#ifndef GESTOR_MEMORIA_H
+#define GESTOR_MEMORIA_H
+
+void asignarMemoria();
+void liberarMemoria();
+void mostrarMemoria();
+
+#endif
